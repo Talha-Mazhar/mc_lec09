bda287978502f390dab4d98ebf415e3d99f877dd
Talha Mazhar <ahmadnutt9111@gmail.com>
2023-01-15 19:05:45 +0500

    Intent For Changing Activity

--- app/src/main/java/com/example/lec09/MainActivity.java
+++ app/src/main/java/com/example/lec09/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.lec09;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1, btn2;
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intt = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intt);
+            }
+        });
     }
 }
